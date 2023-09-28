54eab94e33464361ffce427c11d66e59c6b718c0
KVirB <dlaydrygaakk@mail.ru>
2023-09-28 10:40:06 +0300

    addBrowserRoute

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import CV from "./pages/CV";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CV />
+    <BrowserRouter basename="/CV">
+      <CV />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
